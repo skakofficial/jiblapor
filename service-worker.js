465083c3413b4b66e5b212dccb5c83e721c521e3
skakofficial <sul.astrinot@gmail.com>
2024-10-31 10:11:18 +0000

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,52 @@
+// service-worker.js
+
+// Cache nama
+const CACHE_NAME = 'v1';
+
+// File yang akan di-cache
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/icon-192x192.png',
+    '/icon-512x512.png',
+    'https://unpkg.com/leaflet/dist/leaflet.css',
+    'https://unpkg.com/leaflet/dist/leaflet.js',
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Fetch dari cache
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
+
+// Update cache
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
